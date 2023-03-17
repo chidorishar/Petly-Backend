1f4f15acd198ed21c666f229b2be61378a55db8e
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-18 01:33:17 +0200

    Removed leftovers & formatting

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
 
-// const { YOURrouterHERE } = require('./routes/api');
-const authRouter = require('./routes/api/auth')
-const app = express();
+const authRouter = require('./routes/api/auth');
 
+const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
 
 app.use(logger(formatsLogger));
@@ -15,8 +14,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use('/api/YOURrouterHERE ', YOURrouterHERE);
-app.use("/api/auth", authRouter);
+app.use('/api/auth', authRouter);
+
 app.use((err, req, res, next) => {
   const { status = 500, message = 'Server error' } = err;
   res.status(status).json({ message });
