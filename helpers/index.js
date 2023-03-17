a8a1902bfced91da1ae4a811393c658db5d691d0
Victor-Rochnyak <victor.rochnyak@gmail.com>
2023-03-17 22:53:00 +0200

    Прописав Регістр, Логін користувача

--- helpers/index.js
+++ helpers/index.js
@@ -1,5 +1,6 @@
 const mongooseErrorHandler = require('./mongooseErrorHandler');
-
+const sendEmail = require('./sendEmail')
 module.exports = {
   mongooseErrorHandler,
+  sendEmail
 };
---
+++ models/user.js
@@ -0,0 +1,13 @@
+
+
+
+
+
+// Хеш пароля при регістрації
+userSchema.methods.setPassword = function (password) {
+    this.password = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  };
+// Хеш пароля при логіні
+  userSchema.methods.comparePassword = function (password) {
+    return bcrypt.compareSync(password, this.password);
+  };
