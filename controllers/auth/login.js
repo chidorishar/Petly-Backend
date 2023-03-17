a4955aa39ac06a1f650c6da717fb44a2198d7694
Victor-Rochnyak <victor.rochnyak@gmail.com>
2023-03-17 23:13:10 +0200

    виправив помилку

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -19,7 +19,7 @@ const login = async (req, res) => {
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "1h" });
   await User.findByIdAndUpdate(user._id, { token });
   res.json({
-    ststus: "success",
+    status: "success",
     code: 200,
     data: {
       token,
