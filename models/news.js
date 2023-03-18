b56806a5ffc2d27c2639bd726a8d452cf0b613ef
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-18 16:55:45 +0200

    Chaged "date" field type to Date & added validation for url-links

--- models/news.js
+++ models/news.js
@@ -1,27 +1,34 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require('mongoose');
 
 const newsSchema = Schema(
   {
     title: {
       type: String,
-      required: [true, "Set name for contact"],
+      required: [true, 'Please, add title to your news'],
       unique: true,
+      description: 'News title',
     },
     description: {
       type: String,
+      required: false,
+      description: 'News description',
     },
     date: {
-      type: String,
-      default: "12/11/2022",
+      type: Date,
+      default: Date.now,
+      description: 'News creation date',
     },
     link: {
       type: String,
+      match:
+        /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,
+      description: 'Link to full news',
     },
   },
   { versionKey: false }
 );
 
-const News = model("news", newsSchema);
+const News = model('news', newsSchema);
 
 module.exports = {
   News,
