1562d9bc4edfe9306b970f47cf837c23672aa20b
Oleksandr Chyhryn <as.chigrin@gmail.com>
2023-03-18 22:25:51 +0200

    Add new userServices, make changes in controllers login and register, and resolve conflicts with main

--- controllers/auth/login.js
+++ controllers/auth/login.js
@@ -1,5 +1,5 @@
-const { User } = require('../../models');
 const { InternalServerError, Unauthorized } = require('http-errors');
+const { userServices } = require('../../services');
 
 const jwt = require('jsonwebtoken');
 
@@ -7,7 +7,7 @@ const { SECRET_KEY } = process.env;
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  const user = await User.findOne({ email });
+  const user = await userServices.findUser({ email });
 
   if (!user || !user.comparePassword(password)) {
     throw new Unauthorized('Email or password is wrong ');
@@ -17,7 +17,7 @@ const login = async (req, res) => {
     id: user._id,
   };
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '1h' });
-  const usr = await User.findByIdAndUpdate(user._id, { token });
+  const usr = await userServices.updateUserById(user._id, { token });
   if (!usr) throw new InternalServerError('Failed to save new user');
 
   res.json({
--- services/users.js
+++ services/users.js
@@ -1,7 +1,15 @@
 const { User } = require('../models');
 
+const createUser = ({ ...arg }) => {
+  return new User(arg);
+};
+
 const findUser = ({ ...arg }) => {
   return User.findOne(arg);
 };
 
-module.exports = { findUser };
+const updateUserById = (id, data) => {
+  return User.findByIdAndUpdate(id, data);
+};
+
+module.exports = { createUser, findUser, updateUserById };
