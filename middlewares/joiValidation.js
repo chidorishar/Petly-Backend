807ad07319f9f6b66aa435da41fb264ef075544b
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-18 17:49:39 +0200

    If errorMessage parameter is not provided for middleware use message from error obj

--- middlewares/joiValidation.js
+++ middlewares/joiValidation.js
@@ -8,7 +8,7 @@ const validateWithJoiSchema = (
   const func = (req, res, next) => {
     const data = isBodyValidation ? req.body : req.query;
     const { error } = schema.validate(data);
-    if (error) next(new BadRequest(errorMessage));
+    if (error) next(new BadRequest(errorMessage ?? error.message));
 
     next();
   };
@@ -16,11 +16,11 @@ const validateWithJoiSchema = (
   return func;
 };
 
-const validateBody = (schema, errorMessage = 'missing fields') => {
+const validateBody = (schema, errorMessage) => {
   return validateWithJoiSchema(schema, true, errorMessage);
 };
 
-const validateQueryParams = (schema, errorMessage = 'missing fields') => {
+const validateQueryParams = (schema, errorMessage) => {
   return validateWithJoiSchema(schema, false, errorMessage);
 };
 
