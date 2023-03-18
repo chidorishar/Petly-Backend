5f81f4fd685fabcc68afd0f2486cc7ff8b9c6380
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-18 17:07:19 +0200

    Prettier single comma auto-set

--- routes/api/news.js
+++ routes/api/news.js
@@ -1,11 +1,11 @@
-const express = require("express");
+const express = require('express');
 
-const  ctrl  = require("../../controllers/news");
+const ctrl = require('../../controllers/news');
 
 const router = express.Router();
 
 const { ctrlWrapper } = require('../../middlewares');
 
-router.get("/", ctrlWrapper(ctrl.getAllNews));
+router.get('/', ctrlWrapper(ctrl.getAllNews));
 
 module.exports = router;
