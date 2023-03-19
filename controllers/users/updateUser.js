cd97766df986cff30a5abc9377d833a9be88ebc6
OleksandrB93 <barabashsan4ik@gmail.com>
2023-03-19 20:01:37 +0200

    remove _id

--- controllers/users/updateUser.js
+++ controllers/users/updateUser.js
@@ -1,20 +1,16 @@
 const { User } = require('../../models/users');
-const ObjectId = require('mongodb').ObjectId;
 const { NotFound } = require('http-errors');
 
 const updateUser = async (req, res) => {
   const owner = req.user;
-  const _id = ObjectId(req.params.id);
-
   const user = await User.findOneAndUpdate(
     { _id: owner },
     { $set: req.body },
     { new: true }
     );
 
-    
   if (!user) {
-    throw new NotFound(`Contact with id=${_id} not found`);
+    throw new NotFound(`Contact with id=${owner} not found`);
   }
 
   res.json(user);
