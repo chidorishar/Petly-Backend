b3e14f4c28955252231155e09da1ffdef0c3b810
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-20 21:48:40 +0200

    Tweaked updateUser code

--- controllers/users/updateUser.js
+++ controllers/users/updateUser.js
@@ -2,14 +2,11 @@ const { NotFound } = require('http-errors');
 const { userServices } = require('../../services');
 
 const updateUser = async (req, res) => {
-  const owner = req.user;
-  const user = await userServices.updateUserById(
-    { _id: owner },
-    { $set: req.body },
-    );
+  const ownerID = req.user;
+  const user = await userServices.updateUserById(ownerID, req.body);
 
   if (!user) {
-    throw new NotFound(`Contact with id=${owner} not found`);
+    throw new NotFound(`Contact with id=${ownerID} not found`);
   }
 
   res.json(user);
