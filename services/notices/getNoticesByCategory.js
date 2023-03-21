f5c8a3380d1bd774c0d4a79d53bc32348436a963
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-21 17:53:08 +0200

    Removed "owner" field from responces with Notices

--- services/notices/getNoticesByCategory.js
+++ services/notices/getNoticesByCategory.js
@@ -1,16 +1,16 @@
- const { Notice } = require('../../models');
- const { BadRequest } = require('http-errors');
+const { Notice } = require('../../models');
+const { BadRequest } = require('http-errors');
 
-const getNoticesByCategory = async (category,  { skip, limit }) => {
-    
-    const notices = await Notice.find({ category })
+const getNoticesByCategory = async (category, { skip, limit }) => {
+  const notices = await Notice.find({ category })
     .skip(skip)
-    .limit(limit);
-    
-    if (!notices) {
-        throw new BadRequest(`Not found such category ${category}`);
-      }
-    return notices;
-}
+    .limit(limit)
+    .select('-owner');
+
+  if (!notices) {
+    throw new BadRequest(`Not found such category ${category}`);
+  }
+  return notices;
+};
 
 module.exports = getNoticesByCategory;
