d7db1d0ce1c8d749dae061787cbbebfcb995d084
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-23 23:08:01 +0200

    Fixed bug where auth user can delete notices created by other users

--- services/notices/deleteOwnNotice.js
+++ services/notices/deleteOwnNotice.js
@@ -1,12 +1,20 @@
 const { User, Notice } = require('../../models');
 // const { BadRequest } = require('http-errors');
 
-const deleteOwnNotice = async (id, owner) => {
-  const notice = await Notice.findByIdAndRemove({ _id: id, owner });
-  // console.log(owner)
-  await User.findOneAndUpdate({ _id: owner }, { $pull: { notices: id } });
-  // console.log(notices);
-  // return {notice, notices}
+const deleteOwnNotice = async (noticeID, ownerID) => {
+  // try to find the notice which is owned by a user with ownerID and remove it from notices DB
+  const notice = await Notice.findOneAndRemove({
+    _id: noticeID,
+    owner: ownerID,
+  });
+
+  // if notice was removed from notices DB then remove it from favorites for all users
+  if (notice)
+    await User.updateMany(
+      { favoriteNotices: noticeID },
+      { $pull: { favoriteNotices: noticeID } }
+    );
+
   return notice;
 };
 
