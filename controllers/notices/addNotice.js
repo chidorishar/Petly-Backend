8e0d28a6cd92234fbd71007527b9d519483e3cf4
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-24 00:17:18 +0200

    Store cloudinary ID for notice's uploaded image

--- controllers/notices/addNotice.js
+++ controllers/notices/addNotice.js
@@ -1,13 +1,20 @@
-const service = require("../../services/notices");
+const service = require('../../services/notices');
 
-const addNotice = async(req, res) => { 
-    // to do  обработка корректности даты
-    const image = req.url;
-    const userId = req.user;
-    const data = req.body;
-    
-    const notice = await service.addNotice(userId, data, image);
-    res.status(201).json(notice);
-}
+const addNotice = async (req, res) => {
+  // to do  обработка корректности даты
+  const pathToImage = req.url;
+  const imageCloudinaryID = req.public_id;
+  const userId = req.user;
+  const data = req.body;
+
+  const notice = await service.addNotice(
+    userId,
+    data,
+    pathToImage,
+    imageCloudinaryID
+  );
+
+  res.status(201).json(notice);
+};
 
 module.exports = addNotice;
