c814f001cb3f1e7920407864bdba490a0a6a140d
Victor Nikolenko <45390911+victor1982nik@users.noreply.github.com>
2023-03-23 18:07:42 +0200

    no console errors

--- services/notices/getNoticesByTitle.js
+++ services/notices/getNoticesByTitle.js
@@ -1,5 +1,5 @@
-const { Notice, User } = require('../../models');
-const { BadRequest } = require('http-errors');
+// const { Notice, User } = require('../../models');
+// const { BadRequest } = require('http-errors');
 
 const getNoticesByTitle = async (category, query, { skip, limit }, userId) => {
   
