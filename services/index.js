6c14940829a5589a12a02e9bcaafb18ad3699514
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-23 20:51:55 +0200

    Added a function for generating correct pagination values for mongoose

--- services/index.js
+++ services/index.js
@@ -3,14 +3,15 @@ const { findServices } = require('./services');
 const userServices = require('./users');
 const upload = require('./upload');
 const petServices = require('./pets');
-const parseUserToken = require('./parseUserToken')
-
+const parseUserToken = require('./parseUserToken');
+const parsePagination = require('./parsePagination');
 
 module.exports = {
   connectToMongo,
   userServices,
   upload,
   petServices,
-  findServices ,
-  parseUserToken 
+  findServices,
+  parseUserToken,
+  ...parsePagination,
 };
