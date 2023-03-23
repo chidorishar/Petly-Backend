0a880b6727a5996fcb145104e2bcbbd3c06232da
chidorishar <13747383+chidorishar@users.noreply.github.com>
2023-03-23 17:44:11 +0200

    Merge

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const noticeRouter = require('./routes/api/notices');
 const { usersRouter } = require('./routes/api/users');
 const servicesRouter = require('./routes/api/services');
 require('./services/cloudinary/setCloudinaryConfig');
+// const newsRouter = require('./routes/api/news');
 
 const app = express();
 const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
