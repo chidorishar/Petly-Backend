19444c1967eb865d99bcb1b70084e02746a9209e
OleksandrB93 <barabashsan4ik@gmail.com>
2023-03-24 09:43:35 +0200

    add destroy

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -5,9 +5,10 @@ const fs = require('fs/promises');
 const updateAvatar = async (req, res) => {
   const ownerID = req.user;
   const { path: tempUpload } = req.file;
-  const { url } = await cloudinary.uploader.upload(tempUpload);
+  const { url, public_id } = await cloudinary.uploader.upload(tempUpload);
   const avatarURL = url;
   await User.findByIdAndUpdate(ownerID, { avatarURL });
+  await cloudinary.uploader.destroy(public_id);
   fs.unlink(tempUpload);
   res.json({
     avatarURL,
