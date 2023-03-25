ea88ba29b87f16dcf0f099f5d96234f8bf858a14
myroslavsuprun <sirmiroslavsuprun@gmail.com>
2023-03-25 23:19:40 +0200

    ref: Extract token creation into a function.

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,9 +1,7 @@
 const { InternalServerError, Conflict } = require('http-errors');
+
+const { createToken } = require('services/utils/createToken');
 const { userServices } = require('../../services');
-
-const jwt = require('jsonwebtoken');
-
-const { SECRET_KEY, NODE_ENV } = process.env;
 
 const register = async (req, res) => {
   const { name, email, password, location, phone } = req.body;
@@ -22,12 +20,7 @@ const register = async (req, res) => {
   newUser.setPassword(password);
   newUser.setAvatar(null, '');
 
-  const payload = {
-    id: newUser._id,
-  };
-
-  const tokenLifetime = NODE_ENV === 'development' ? '1w' : '1h';
-  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: tokenLifetime });
+  const token = createToken({ id: newUser._id });
   newUser.setToken(token);
 
   const savedUser = await newUser.save();
